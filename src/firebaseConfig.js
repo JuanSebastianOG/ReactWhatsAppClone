7b7e2d846d7ef4b79f38705db75ce300c7e54221
Juan Sebastian <seposorio@hotmail.com>
2020-08-18 14:28:40 -0500

    First steps - Clean up

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9Su40uKPLzX-sWMDzaxyw7FUwIr_JQ4E",
+    authDomain: "whatsapp-clone-3a598.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-3a598.firebaseio.com",
+    projectId: "whatsapp-clone-3a598",
+    storageBucket: "whatsapp-clone-3a598.appspot.com",
+    messagingSenderId: "562885733461",
+    appId: "1:562885733461:web:460320d54659e8846af8dc",
+    measurementId: "G-L4L99JHN7M"
+  };
